47f90c028296d1d7b91850840040a72b6e7a12d8
Mody <148470115+modymesourceze@users.noreply.github.com>
2024-01-20 18:09:11 +0200

    Add files via upload

---
+++ mody.py
@@ -0,0 +1,12 @@
+import os
+
+
+class Mody(object):
+    ELHYBA = os.environ.get("ELHYBA", "")
+    
+    API_ID = os.environ.get("API_ID", "")
+    
+    API_HASH = os.environ.get("API_HASH", "")
+    
+    
+
